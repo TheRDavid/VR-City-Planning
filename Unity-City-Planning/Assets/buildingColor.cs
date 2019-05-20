eb858fe89184639b6efb11802b0dd32e9f56f0b9
Justin <j.m.krooneman@student.utwente.nl>
2019-05-20 23:41:35 +0200

    Adjustable color for the house
    Eventhough it always becomes pink

---
+++ Unity-City-Planning/Assets/buildingColor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildingColor : MonoBehaviour
+{
+
+    Color groenig = new Color32(10, 200, 0, 0);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Renderer rend = gameObject.GetComponentsInChildren<Renderer>()[0];
+        rend.material.shader = Shader.Find("_Color");
+        rend.material.SetColor("_Color", groenig);
+    }
+}
