3f44fe950a2dace82bab0cefe31a45bcebdb34fc
Annkatrin Jung <anni@ujung.de>
2019-06-12 13:53:05 +0200

    calculate natureAccess

--- Unity-City-Planning/Assets/Scripts/Logic/Data/Municipality.cs
+++ Unity-City-Planning/Assets/Scripts/Logic/Data/Municipality.cs
@@ -136,12 +136,10 @@ public class Municipality
     }
 
     public void calculateNatureAccess(){
-        int peopleWithAccess = 0;
+        // we are currently not tracking the location of greenspace
+        // so instead, we calculate the proportion of greenspace per population
 
-        foreach (Building b in this.buildings)
-        {
-
-        }
+        natureAccess = greenSpace / totalPopulation;
     }
 
 
